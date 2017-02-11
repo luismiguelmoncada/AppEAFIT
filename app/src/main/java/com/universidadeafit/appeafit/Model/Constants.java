e8bd93f4b460faafe019c62debc56301be83d71a
luismiguelmoncada <luis05247@gmail.com>
2017-02-11 18:05:52 -0500

    Definicion del Modelo

---
+++ app/src/main/java/com/universidadeafit/appeafit/Model/Constants.java
@@ -0,0 +1,20 @@
+package com.universidadeafit.appeafit.Model;
+
+/**
+ * Created by LUISM on 03/01/2017.
+ */
+
+public class Constants {
+
+    //http://192.168.0.109:64432/   /api/retrofit_users
+    //http://localhost:64432/   /api/retrofit_users
+    //http://10.0.3.2:64432/    /api/retrofit_users para conexion con Genymotion
+
+    public static final String BASE_URL = "http://webserverandroid.site88.net/"; //http://192.168.0.109:64432//api/retrofit_users
+    public static final String REGISTER_OPERATION = "register";
+    public static final String LOGIN_OPERATION = "login";
+
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+    public static final String IS_LOGGED_IN = "isLoggedIn";
+}
