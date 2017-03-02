4b52566f79d3111b7be3f94fd5b63700ee4700c5
luismiguelmoncada <luis05247@gmail.com>
2017-03-02 08:50:59 -0500

    Test CHATBOT, WATSON

---
+++ app/src/main/java/com/universidadeafit/appeafit/Views/WatsonActivity.java
@@ -0,0 +1,15 @@
+package com.universidadeafit.appeafit.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.universidadeafit.appeafit.R;
+
+public class WatsonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watson);
+    }
+}
